15b1cd14d8fa791e7a317a4ae7b0b6c9bb2076d6
RRiverWest <null>
2024-12-13 23:44:38 +0900

    add: rust config

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,3 +1,5 @@
+local lspconfig = require("lspconfig")
+
 require("mason").setup()
 require("mason-lspconfig").setup({
 	ensure_installed = { "lua_ls", "clangd", "pylsp" },
@@ -10,6 +12,16 @@ require("mason-lspconfig").setup_handlers({
 	end,
 })
 
+lspconfig.rust_analyzer.setup({
+  settings = {
+    ["rust-analyzer"] = {
+      check = {
+        command = "clippy",
+      },
+    },
+  },
+})
+
 local cmp = require("cmp")
 local lspkind = require("lspkind")
 cmp.setup({
@@ -18,11 +30,8 @@ cmp.setup({
 			format = lspkind.cmp_format({
 				mode = 'symbol',
 				maxwidth = 50,
-				ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-				show_labelDetails = true,            -- show labelDetails in menu. Disabled by default
-
-				-- The function below will be called before any actual modifications from lspkind
-				-- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+				ellipsis_char = '...',
+				show_labelDetails = true,
 				before = function(entry, vim_item)
 					return vim_item
 				end
