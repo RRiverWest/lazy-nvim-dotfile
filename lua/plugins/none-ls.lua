fb6b163b31952da89906463994b0ebc73d3c4ddc
RRiverWest <kawa.ryo.0126@gmail.com>
2025-06-22 12:12:13 +0900

    delete: mason-null-ls

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,17 +1,22 @@
 return {
-	"jay-babu/mason-null-ls.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	dependencies = {
-		"williamboman/mason.nvim",
+	{
 		"nvimtools/none-ls.nvim",
+		requires = {
+			"nvimtools/none-ls-extras.nvim",
+			"nvim-lua/plenary.nvim",
+		},
+		config = function()
+			local null_ls = require("null-ls")
+
+			null_ls.setup({
+				sources = {
+					null_ls.builtins.formatting.clang_format,
+					null_ls.builtins.diagnostics.cppcheck,
+					null_ls.builtins.formatting.stylua,
+					-- null_ls.builtins.diagnostics.cppcheck,
+					null_ls.builtins.formatting.prettierd,
+				},
+			})
+		end,
 	},
-	config = function()
-		require("mason").setup()
-		require("mason-null-ls").setup({
-			ensure_installed = { "stylua", },
-			automatic_installation = false,
-			automatic_setup = true,
-			handlers = {},
-		})
-	end,
 }
