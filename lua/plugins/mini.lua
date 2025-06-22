ef41cafb284f9f4ce136eab64df1df96a2e008ea
RRiverWest <kawa.ryo.0126@gmail.com>
2025-06-22 12:14:23 +0900

    add: cursorword config

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,7 +1,21 @@
 return {
 	"echasnovski/mini.nvim",
 	config = function()
-		require('mini.cursorword').setup()
-		require('mini.move').setup()
+		require("mini.cursorword").setup()
+
+		require("mini.move").setup()
+		require("mini.move").setup({
+			mappings = {
+				left = "H",
+				right = "L",
+				down = "J",
+				up = "K",
+
+				line_left = "<S-left>",
+				line_right = "<S-right>",
+				line_down = "<S-down>",
+				line_up = "<S-up>",
+			},
+		})
 	end,
 }
