ef90698840213ccf96ea5d2d92cf2ca2410f01cc
RRiverWest <kawa.ryo.0126@gmail.com>
2025-12-08 19:42:18 +0900

    change: enable dotfile

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -3,19 +3,27 @@ return {
 		"nvim-tree/nvim-tree.lua",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		config = function()
-			require("nvim-tree").setup {
+			require("nvim-tree").setup({
 				view = {
 					width = 25,
-					side = 'left',
+					side = "left",
 				},
 				renderer = {
-					highlight_opened_files = 'all',
+					highlight_opened_files = "all",
 				},
 				filters = {
-					dotfiles = true,
+					dotfiles = false,
 				},
-			}
+			})
+			vim.api.nvim_create_autocmd("BufEnter", {
+				nested = true,
+				callback = function()
+					if #vim.api.nvim_list_wins() == 1 and require("nvim-tree.utils").is_nvim_tree_buf() then
+						vim.cmd("quit")
+					end
+				end,
+			})
 		end,
 		event = "BufWinEnter",
-	}
+	},
 }
