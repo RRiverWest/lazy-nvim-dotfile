7df3a47ea44f226454953d2453d46c15aa1369cb
RRiverWest <kawa.ryo.0126@gmail.com>
2025-12-08 19:41:30 +0900

    change linter formatter

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -11,7 +11,8 @@ return {
 			null_ls.setup({
 				sources = {
 					null_ls.builtins.formatting.clang_format,
-					null_ls.builtins.diagnostics.cppcheck,
+					-- null_ls.builtins.diagnostics.cppcheck,
+					-- null_ls.builtins.diagnostics.eslint_d,
 					null_ls.builtins.formatting.stylua,
 					-- null_ls.builtins.diagnostics.cppcheck,
 					null_ls.builtins.formatting.prettierd,
